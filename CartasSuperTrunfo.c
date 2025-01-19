c174ced45f26513786e4cc3cb4d23b098cff8dd6
Kleyciane Dutra Mendes <160146713+MajorKley@users.noreply.github.com>
2025-01-19 16:20:15 +0000

    Novo modo de organização das informações.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,132 +1,95 @@
 #include <stdio.h>
 
+//%[^\n]
+
 int main() {
 
-   char A[20], B[20],C[20],D[20],E[20],F[20],G[20],H[20],I[20],J[20];
-    char A01[20],A02[20], A03[20],A04[20];
-    char B01[20],B02[20], B03[20],B04[20];
-    char C01[20],C02[20], C03[20],C04[20];
-    char D01[20],D02[20], D03[20],D04[20];
-    char E01[20],E02[20], E03[20],E04[20];
-    char F01[20],F02[20], F03[20],F04[20];
-    char G01[20],G02[20], G03[20],G04[20];
-    char H01[20],H02[20], H03[20],H04[20];
-    char I01[20],I02[20], I03[20],I04[20];
-    char J01[20],J02[20], J03[20],J04[20];
+char Estado[20];
+char Cid [20];
+float populacao, pib, area;
+int turismo; //pontos turísticos
 
-   printf("Vamos jogar super trunfo? Iremos começar escolhendo os estados:\n");
-   printf("Escolha o estado A:");
-   scanf(" %s", A);
-   printf("Escolha o estado B:");
-   scanf(" %s", B);
-   printf("Escolha o estado C:");
-   scanf(" %s", C);
-   printf("Escolha o estado D:");
-   scanf(" %s", D);
-   printf("Escolha o estado E:");
-   scanf(" %s", E);
-   printf("Escolha o estado F:");
-   scanf(" %s", F);
-   printf("Escolha o estado G:");
-   scanf(" %s", G);
-   printf("Escolha o estado H:");
-   scanf(" %s", H);
-   printf("Escolha o estado I:");
-   scanf(" %s", I);
-   printf("Escolha o estado J:");
-   scanf(" %s", J);
-   
-   printf("Muito bem! Agora iremos cadastrar 04 cidades para cada estado: \n");
-   printf("Digite a 01 do estado %s :", A);
-   scanf("%s", A01);
-   printf("Digite a 02 do estado %s :", A);
-   scanf("%s", A02);
-   printf("Digite a 03 do estado %s :", A);
-   scanf("%s", A03);
-   printf("Digite a 04 do estado %s :", A);
-   scanf("%s", A04);
+printf("Vamos jogar Super Trunfo Paises! Vamos começar cadastrando suas cartas. \n");
+printf("Digite um estado: \n");
+scanf("%[^\n]", Estado);
 
-   printf("Digite a 01 do estado %s :", B);
-   scanf("%s", B01);
-   printf("Digite a 02 do estado %s :", B);
-   scanf("%s", B02);
-   printf("Digite a 03 do estado %s :", B);
-   scanf("%s", B03);
-   printf("Digite a 04 do estado %s :", B);
-   scanf("%s", B04);
-   
-   printf("Digite a 01 do estado %s :", C);
-   scanf("%s", C01);
-   printf("Digite a 02 do estado %s :", C);
-   scanf("%s", C02);
-   printf("Digite a 03 do estado %s :", C);
-   scanf("%s", C03);
-   printf("Digite a 04 do estado %s :", C);
-   scanf("%s", C04);
-   
-   printf("Digite a 01 do estado %s :", D);
-   scanf("%s", D01);
-   printf("Digite a 02 do estado %s :", D);
-   scanf("%s", D02);
-   printf("Digite a 03 do estado %s :", D);
-   scanf("%s", D03);
-   printf("Digite a 04 do estado %s :", D);
-   scanf("%s", D04);
+    printf("Agora vamos cadastrar as 04 cidades \n");
+    printf("Digite a 01 cidade do estado %s:\n", Estado);
+    scanf(" %[^\n]%*c", &Cid);
+        printf("Digite a Populaçao: \n");
+        scanf("%f", &populacao);
+        printf("Digite a Área:\n");
+        scanf("%f", &area);
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib);
+        printf("Digite o Número de pontos turísticos da cidade: \n");
+        scanf("%d", &turismo);
 
-    printf("Digite a 01 do estado %s :", E);
-   scanf("%s", E01);
-   printf("Digite a 02 do estado %s :", E);
-   scanf("%s", E02);
-   printf("Digite a 03 do estado %s :", E);
-   scanf("%s", E03);
-   printf("Digite a 04 do estado %s :", E);
-   scanf("%s", E04);
+            printf("A cidade %s do estado %s possue os seguintes dados: \n", Cid, Estado);
+            printf("Populaçao: %f \n", populacao);
+            printf("Área: %f \n", area);
+            printf("PIB: %f \n", pib);
+            printf("Número de Pontos Turísticos:  %d \n", turismo);
+          
+            
+    printf("Digite a 02 cidade do estado %s:\n", Estado);
+    scanf(" %[^\n]%*c", &Cid);
+        printf("Digite a Populaçao: \n");
+        scanf("%f", &populacao);
+        printf("Digite a Área:\n");
+        scanf("%f", &area);
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib);
+        printf("Digite o Número de pontos turísticos da cidade: \n");
+        scanf("%d", &turismo);
 
-    printf("Digite a 01 do estado %s :", F);
-   scanf("%s", F01);
-   printf("Digite a 02 do estado %s :", F);
-   scanf("%s", F02);
-   printf("Digite a 03 do estado %s :", F);
-   scanf("%s", F03);
-   printf("Digite a 04 do estado %s :", F);
-   scanf("%s", F04);
+            printf("A cidade %s do estado %s possue os seguintes dados: \n", Cid, Estado);
+            printf("Populaçao: %f \n", populacao);
+            printf("Área: %f \n", area);
+            printf("PIB: %f \n", pib);
+            printf("Número de Pontos Turísticos:  %d \n", turismo);
+            printf("O número da carta será A02");
 
-    printf("Digite a 01 do estado %s :", G);
-   scanf("%s", G01);
-   printf("Digite a 02 do estado %s :", G);
-   scanf("%s", G02);
-   printf("Digite a 03 do estado %s :", G);
-   scanf("%s", G03);
-   printf("Digite a 04 do estado %s :", G);
-   scanf("%s", G04);
+    printf("Digite a 03 cidade do estado %s:\n", Estado);
+    scanf(" %[^\n]%*c", &Cid);
 
-    printf("Digite a 01 do estado %s :", H);
-   scanf("%s", H01);
-   printf("Digite a 02 do estado %s :", H);
-   scanf("%s", H02);
-   printf("Digite a 03 do estado %s :", H);
-   scanf("%s", H03);
-   printf("Digite a 04 do estado %s :", H);
-   scanf("%s", H04);
+        printf("Digite a Populaçao: \n");
+        scanf("%f", &populacao);
+        printf("Digite a Área:\n");
+        scanf("%f", &area);
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib);
+        printf("Digite o Número de pontos turísticos da cidade: \n");
+        scanf("%d", &turismo);
 
-    printf("Digite a 01 do estado %s :", I);
-   scanf("%s", I01);
-   printf("Digite a 02 do estado %s :", I);
-   scanf("%s", I02);
-   printf("Digite a 03 do estado %s :", I);
-   scanf("%s", I03);
-   printf("Digite a 04 do estado %s :", I);
-   scanf("%s", I04);
+            printf("A cidade %s do estado %s possue os seguintes dados: \n", Cid, Estado);
+            printf("Populaçao: %f \n", populacao);
+            printf("Área: %f \n", area);
+            printf("PIB: %f \n", pib);
+            printf("Número de Pontos Turísticos:  %d \n", turismo);
+            printf("O número da carta será A03");
 
-    printf("Digite a 01 do estado %s :", J);
-   scanf("%s", J01);
-   printf("Digite a 02 do estado %s :", J);
-   scanf("%s", J02);
-   printf("Digite a 03 do estado %s :", J);
-   scanf("%s", J03);
-   printf("Digite a 04 do estado %s :", J);
-   scanf("%s", J04);
+    printf("Digite a 04 cidade do estado %s:\n", Estado);
+    scanf(" %[^\n]%*c", &Cid);
 
-   
+        printf("Digite a Populaçao: \n");
+        scanf("%f", &populacao);
+        printf("Digite a Área:\n");
+        scanf("%f", &area);
+        printf("Digite o PIB: \n");
+        scanf("%f", &pib);
+        printf("Digite o Número de pontos turísticos da cidade: \n");
+        scanf("%d", &turismo);
+
+            printf("A cidade %s do estado %s possue os seguintes dados: \n", Cid, Estado);
+            printf("Populaçao: %f \n", populacao);
+            printf("Área: %f \n", area);
+            printf("PIB: %f \n", pib);
+            printf("Número de Pontos Turísticos:  %d \n", turismo);
+            printf("O número da carta será A04 \n");
+
+        
+    printf("Para cadastrar as outras cartas, reinicie o processo.");
+    
     return 0;
 }
